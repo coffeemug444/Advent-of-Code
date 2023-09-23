0ce47070cd5c126913d30af078aeef0fde7c8299
Ben Slattery <ben@benslattery.com>
2023-09-23 21:12:48 +1200

    2022 day 6 part 2

--- 2022/day_06/part2.cpp
+++ 2022/day_06/part2.cpp
@@ -2,11 +2,26 @@
 #include "../../helper_functions.hpp"
 
 
+const unsigned MARKER_LEN = 14;
 
 
-
+bool is_marker(unsigned pos, std::string_view message)
+{
+   std::set<char> chars;
+   for (unsigned i = 0; i < MARKER_LEN; i++) chars.insert(message.at(pos - i));
+   return chars.size() == MARKER_LEN;
+}
 
 int main()
 {
-   std::vector<std::string> lines = open_lines("data.dat");
+   std::string message = open_lines("data.dat").front();
+
+   unsigned pos = MARKER_LEN;
+   while (not is_marker(pos, message))
+   {
+      pos++;
+      assert(pos < message.size() && "No marker found");
+   }
+
+   std::cout << pos + 1 << std::endl;
 }
