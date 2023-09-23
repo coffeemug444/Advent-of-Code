1de606d29348af113773f8b8f43ca7ac2db252be
Ben Slattery <ben@benslattery.com>
2023-09-23 16:49:38 +1200

    2022 day 1 p2

--- 2022/day_01/part2.cpp
+++ 2022/day_01/part2.cpp
@@ -3,10 +3,29 @@
 
 
 
-
-
-
 int main()
 {
    std::vector<std::string> lines = open_lines("data.dat");
+
+   std::array<unsigned, 3> top_three {};
+   unsigned total = 0;
+   for (auto& line : lines)
+   {
+      if (line == "")
+      {
+         unsigned& worst_elf = *std::min_element(top_three.begin(), top_three.end());
+         worst_elf = std::max(worst_elf, total);
+         total = 0;
+         continue;
+      }
+
+      total += std::stoi(line);
+   }
+
+   unsigned worst_elf = *std::min_element(top_three.begin(), top_three.end());
+   worst_elf = std::max(worst_elf, total);
+
+   total = std::accumulate(top_three.begin(), top_three.end(), 0u);
+
+   std::cout << "The total food carried by the best 3 elfs is: " << total << std::endl; 
 }
