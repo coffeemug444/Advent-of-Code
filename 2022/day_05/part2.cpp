304331283ee1e9cda64e633930bec5bdc48f2710
Ben Slattery <ben@benslattery.com>
2023-09-23 20:21:47 +1200

    2022 day 5 part 2

--- 2022/day_05/part2.cpp
+++ 2022/day_05/part2.cpp
@@ -1,12 +1,59 @@
 #include "../../generic_includes.hpp"
 #include "../../helper_functions.hpp"
 
+const unsigned NUMBER_OF_COLS = 9;
 
 
+std::array<std::vector<char>, NUMBER_OF_COLS> parse_initial(const std::vector<std::string>& initial)
+{
+   std::array<std::vector<char>, NUMBER_OF_COLS> box_stacks {};
+   for (auto& row : initial)
+   {
+      for (size_t col = 0; col < NUMBER_OF_COLS; col++)
+      {
+         char c = row.at(1 + 4*col);
+         if (c == ' ') continue;
+         box_stacks.at(col).insert(box_stacks.at(col).begin(), c);
+      }
+   }
 
+   return box_stacks;
+}
+
+std::tuple<unsigned, unsigned, unsigned> parse_command(std::string_view command)
+{
+   auto words = split(command, ' ');
+   unsigned count = std::stoul(words.at(1));
+
+   // subtract 1 from these as we're using 0 indexing not 1
+   unsigned from = std::stoul(words.at(3)) - 1;
+   unsigned to = std::stoul(words.at(5)) - 1;
+   return {count, from, to};
+}
 
 
 int main()
 {
    std::vector<std::string> lines = open_lines("data.dat");
+
+   // end of the starting stack in the input data
+   auto end_of_initial = std::find(lines.begin(), lines.end(), "");
+   std::vector<std::string> initial;
+   std::copy(lines.begin(), end_of_initial, std::back_inserter(initial));
+   lines.erase(lines.begin(), ++end_of_initial);
+   initial.pop_back(); // this is just numbering the columms, not needed
+
+   std::array<std::vector<char>, NUMBER_OF_COLS> box_stacks = parse_initial(initial);
+
+   for (std::string_view command : lines)
+   {
+      auto [count, from, to] = parse_command(command);
+      std::vector<char>& from_stack = box_stacks.at(from);
+      std::vector<char>& to_stack = box_stacks.at(to);
+      to_stack.insert(to_stack.end(), from_stack.end() - count, from_stack.end());
+      from_stack.erase(from_stack.end() - count, from_stack.end());
+   }
+
+   for (auto& box_stack : box_stacks) std::cout << box_stack.back();
+   std::cout << std::endl;
 }
