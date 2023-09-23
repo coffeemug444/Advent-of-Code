cd09f8a472c92d4c53df9f36eb6ca747fbaa9ff6
Ben Slattery <ben@benslattery.com>
2023-09-23 17:50:27 +1200

    2022 day 3 part 1

--- 2022/day_03/part1.cpp
+++ 2022/day_03/part1.cpp
@@ -4,9 +4,34 @@
 
 
 
+unsigned priority(char c)
+{
+   if ('a' <= c and c <= 'z') return 1 + (c - 'a');
+   if ('A' <= c and c <= 'Z') return 27 + (c - 'A');
+   assert(false && "Bad char");
+}
+
+char common_char(std::string_view left, std::string_view right)
+{
+   for (char c : left)
+   {
+      if (right.find(c) != std::string::npos) return c;
+   }
+   assert(false && "No char in left appears in right");
+}
 
 
 int main()
 {
    std::vector<std::string> lines = open_lines("data.dat");
+
+
+   unsigned total = 0;
+   for (auto& line : lines)
+   {
+      size_t line_len = line.size();
+      total += priority(common_char(line.substr(0, line_len / 2), line.substr(line_len/2)));
+   }
+
+   std::cout << total << std::endl;
 }
