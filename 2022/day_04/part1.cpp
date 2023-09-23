e671c462c54b9e2486505310ec26ca22300c72df
Ben Slattery <ben@benslattery.com>
2023-09-23 19:01:05 +1200

    2022 day 4 part 1

--- 2022/day_04/part1.cpp
+++ 2022/day_04/part1.cpp
@@ -3,10 +3,30 @@
 
 
 
-
+std::pair<unsigned, unsigned> string_to_range(std::string_view input)
+{
+   std::vector<std::string> range = split(input, '-');
+   return {std::stoul(range.at(0)), std::stoul(range.at(1))};
+}
 
 
 int main()
 {
    std::vector<std::string> lines = open_lines("data.dat");
+
+   unsigned total = 0;
+   for (auto& line : lines)
+   {
+      std::vector<std::string> pair = split(line, ',');
+      auto [start_1, end_1] = string_to_range(pair.at(0));
+      auto [start_2, end_2] = string_to_range(pair.at(1));
+      if ((start_1 <= start_2 and end_1 >= end_2) or 
+          (start_1 >= start_2 and end_1 <= end_2))
+      {
+         total++;
+      } 
+   }
+
+
+   std::cout << total << std::endl;
 }
