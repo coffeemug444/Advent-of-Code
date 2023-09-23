26160b4b9cb06956cc60f9eb0550d3512cdee650
Ben Slattery <ben@benslattery.com>
2023-09-23 16:48:56 +1200

    2022 p1

--- 2022/day_01/part1.cpp
+++ 2022/day_01/part1.cpp
@@ -3,10 +3,25 @@
 
 
 
-
-
-
 int main()
 {
    std::vector<std::string> lines = open_lines("data.dat");
+
+   unsigned max = 0;
+   unsigned total = 0;
+   for (auto& line : lines)
+   {
+      if (line == "")
+      {
+         max = std::max(total, max);
+         total = 0;
+         continue;
+      }
+
+      total += std::stoi(line);
+   }
+
+   max = std::max(total, max);
+
+   std::cout << "The most an elf is carrying is: " << max << std::endl; 
 }
